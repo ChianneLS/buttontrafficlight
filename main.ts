cf769fc34b09ddae37c18aceeb1636228f989974
Chianne <chiannelyfordshields@gmail.com>
2024-12-13 12:22:45 +1300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,13 @@
+function resetLights () {
+    Environment.ledBrightness(AnalogPin.P1, false)
+    Environment.ledBrightness(AnalogPin.P2, false)
+    Environment.ledBrightness(AnalogPin.P3, false)
+}
+basic.clearScreen()
+basic.showIcon(IconNames.Heart)
+resetLights()
+basic.pause(5000)
+basic.clearScreen()
+basic.forever(function () {
+	
+})
